25b9cd3c394bdca22a522e3ea276eb9b1d184756
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2025-01-20 00:23:13 -0600

    Add code for required fields and methods of the Arena and Tenant classes

---
+++ csc258Review/src/Arena.java
@@ -0,0 +1,69 @@
+public class Arena
+{
+    // Fields
+    private String venueName;
+    private String city;
+    private String state;
+
+    private int maxCapacity;
+    private int yearOpened;
+    private Tenant team;
+
+    // Constructors
+    public Arena() {
+
+    }
+
+    public Arena(String v, String c, String s,
+                 int m, int y,
+                 String teamName, String sport, String league) {
+
+    }
+
+    // Setters
+    public void setVenueName(String v) {
+
+    }
+
+    public void setCity(String c) {
+
+    }
+
+    public void setState(String s) {
+
+    }
+
+    public void setMaxCapacity(int m) {
+
+    }
+
+    public void setYearOpened(int y) {
+
+    }
+
+    // Getters
+    public String getVenueName() {
+        return null;
+    }
+
+    public String getCity() {
+        return null;
+    }
+
+    public String getState() {
+        return null;
+    }
+
+    public int getMaxCapacity() {
+        return 0;
+    }
+
+    public int getYearOpened() {
+        return 0;
+    }
+
+    // toString
+    public String toString() {
+        return String.format("");
+    }
+}
---
+++ csc258Review/src/Tenant.java
@@ -0,0 +1,47 @@
+public class Tenant
+{
+    // Fields
+    private String teamName;
+    private String sport;
+    private String league;
+
+    // Constructors
+    public Tenant() {
+
+    }
+
+    public Tenant(String t, String s, String l) {
+
+    }
+
+    // Setters
+    public void setTeamName(String t) {
+
+    }
+
+    public void setSport(String s) {
+
+    }
+
+    public void setLeague(String l) {
+
+    }
+
+    // Getters
+    public String getTeamName() {
+        return null;
+    }
+
+    public String getSport() {
+        return null;
+    }
+
+    public String getLeague() {
+        return null;
+    }
+
+    // toString
+    public String toString() {
+        return String.format("");
+    }
+}
